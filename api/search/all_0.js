60eeaec60b48f33a383c4eafa33a4e13452ddee5
Mateusz Krawiec <m.krawiec@avsystem.com>
2018-02-12 16:30:04 +0100

    Documentation for Anjay 1.7.2

--- api/search/all_0.js
+++ api/search/all_0.js
@@ -172,7 +172,7 @@ var searchData=
   ['anjay_5fpersistence_5fdouble',['anjay_persistence_double',['../persistence_8h.html#a4105dd534533c782abc5d760bf1eaf50',1,'persistence.h']]],
   ['anjay_5fpersistence_5fhandler_5fcollection_5felement_5ft',['anjay_persistence_handler_collection_element_t',['../persistence_8h.html#aff052e7a8a15f33e150c7b8bf488c34f',1,'persistence.h']]],
   ['anjay_5fpersistence_5fignore_5fcontext_5fnew',['anjay_persistence_ignore_context_new',['../persistence_8h.html#a2354bb916fc68d7feadaa689107f8bb7',1,'persistence.h']]],
-  ['anjay_5fpersistence_5flist',['anjay_persistence_list',['../persistence_8h.html#abed817dd2e9935c78a2fbd2e7b36c6ad',1,'persistence.h']]],
+  ['anjay_5fpersistence_5flist',['anjay_persistence_list',['../persistence_8h.html#a6f83619266c77743086b5cbc4356835a',1,'persistence.h']]],
   ['anjay_5fpersistence_5frestore_5fcontext_5fnew',['anjay_persistence_restore_context_new',['../persistence_8h.html#aa0619258808a3daffd672bb81a3cd07d',1,'persistence.h']]],
   ['anjay_5fpersistence_5fsized_5fbuffer',['anjay_persistence_sized_buffer',['../persistence_8h.html#a3d1c581d2681b68d0853da60423f19d8',1,'persistence.h']]],
   ['anjay_5fpersistence_5fstore_5fcontext_5fnew',['anjay_persistence_store_context_new',['../persistence_8h.html#a445ffb739fcd9476d71b2a721f11ba01',1,'persistence.h']]],
