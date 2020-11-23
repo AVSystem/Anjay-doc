1e587498d438243666460073d7fbcc37b933791c
Mateusz Kwiatkowski <m.kwiatkowski@avsystem.com>
2020-11-23 14:42:59 +0100

    Documentation for Anjay 2.8.0

--- api/search/variables_3.js
+++ api/search/variables_3.js
@@ -1,11 +1,11 @@
 var searchData=
 [
-  ['dane_5ftlsa_5frecord_519',['dane_tlsa_record',['../structanjay__security__config__t.html#aff2a5c435e752ed77657abc518f33d8c',1,'anjay_security_config_t']]],
-  ['default_5fmax_5fperiod_520',['default_max_period',['../structanjay__server__instance__t.html#a9401f437c3fec8397405d20fd7e939f3',1,'anjay_server_instance_t']]],
-  ['default_5fmin_5fperiod_521',['default_min_period',['../structanjay__server__instance__t.html#ac65af5d5277dc0f44cc7466ae4549e74',1,'anjay_server_instance_t']]],
-  ['default_5ftls_5fciphersuites_522',['default_tls_ciphersuites',['../structanjay__configuration.html#ab5c0d3b6618cce9452d224a361e7479f',1,'anjay_configuration']]],
-  ['details_523',['details',['../structanjay__download__status__t.html#a1d44a17e19509d3b31d9fbeb2e58cab7',1,'anjay_download_status_t']]],
-  ['disable_5flegacy_5fserver_5finitiated_5fbootstrap_524',['disable_legacy_server_initiated_bootstrap',['../structanjay__configuration.html#aa5f75a1b0546352b00b4bddb3edab1eb',1,'anjay_configuration']]],
-  ['disable_5ftimeout_525',['disable_timeout',['../structanjay__server__instance__t.html#adeb5992aa2f9dfdc6f4e3fd1a9a58131',1,'anjay_server_instance_t']]],
-  ['dtls_5fversion_526',['dtls_version',['../structanjay__configuration.html#ab32477e7370a36e02db5b7e7ccbdd89d',1,'anjay_configuration']]]
+  ['dane_5ftlsa_5frecord_517',['dane_tlsa_record',['../structanjay__security__config__t.html#aff2a5c435e752ed77657abc518f33d8c',1,'anjay_security_config_t']]],
+  ['default_5fmax_5fperiod_518',['default_max_period',['../structanjay__server__instance__t.html#a9401f437c3fec8397405d20fd7e939f3',1,'anjay_server_instance_t']]],
+  ['default_5fmin_5fperiod_519',['default_min_period',['../structanjay__server__instance__t.html#ac65af5d5277dc0f44cc7466ae4549e74',1,'anjay_server_instance_t']]],
+  ['default_5ftls_5fciphersuites_520',['default_tls_ciphersuites',['../structanjay__configuration.html#ab5c0d3b6618cce9452d224a361e7479f',1,'anjay_configuration']]],
+  ['details_521',['details',['../structanjay__download__status__t.html#a1d44a17e19509d3b31d9fbeb2e58cab7',1,'anjay_download_status_t']]],
+  ['disable_5flegacy_5fserver_5finitiated_5fbootstrap_522',['disable_legacy_server_initiated_bootstrap',['../structanjay__configuration.html#aa5f75a1b0546352b00b4bddb3edab1eb',1,'anjay_configuration']]],
+  ['disable_5ftimeout_523',['disable_timeout',['../structanjay__server__instance__t.html#adeb5992aa2f9dfdc6f4e3fd1a9a58131',1,'anjay_server_instance_t']]],
+  ['dtls_5fversion_524',['dtls_version',['../structanjay__configuration.html#ab32477e7370a36e02db5b7e7ccbdd89d',1,'anjay_configuration']]]
 ];
